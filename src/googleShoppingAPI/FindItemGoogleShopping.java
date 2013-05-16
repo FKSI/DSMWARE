d15e5eaba603ed758865cabc8dd37706cb4bccfe
FKSI <ky.francois.3@gmail.com>
2013-05-16 16:27:48 +0200

    Added GoogleShopping API

---
+++ src/googleShoppingAPI/FindItemGoogleShopping.java
@@ -0,0 +1,54 @@
+package googleShoppingAPI;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FindItemGoogleShopping {
+
+	private static String readUrl(String urlString) throws Exception {
+		BufferedReader reader = null;
+		try {
+			URL url = new URL(urlString);
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			StringBuffer buffer = new StringBuffer();
+			int read;
+			char[] chars = new char[1024];
+			while ((read = reader.read(chars)) != -1)
+				buffer.append(chars, 0, read); 
+
+			return buffer.toString();
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+	}
+
+
+	public static void findItemGoogleShopping (String keywords) throws Exception{
+
+	try {
+			JSONObject json = new JSONObject(readUrl("https://www.googleapis.com/shopping/search/v1/public/products?key=AIzaSyA60ZmMU9vLguggfj35Wp2onuPfM894TWI&country=US&q=" + keywords.replace(" ", "+") ));
+
+			JSONArray items = json.getJSONArray("items");
+
+			for (int i = 0; i < items.length(); i++) {  
+				JSONObject item = items.getJSONObject(i);
+				JSONObject products = item.getJSONObject("product");
+				JSONArray inventories = products.getJSONArray("inventories");
+				JSONObject author = products.getJSONObject("author");
+				JSONObject prices = (JSONObject) inventories.get(0);
+				System.out.println(author.get("name") + " - " + products.get("title").toString() + " - " +  prices.get("price") + prices.get("currency"));
+
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+
+}
---
+++ src/googleShoppingAPI/FindItemGoogleShoppingMain.java
@@ -0,0 +1,15 @@
+package googleShoppingAPI;
+
+public class FindItemGoogleShoppingMain {
+
+	/**
+	 * @param args
+	 * @throws Exception 
+	 */
+	public static void main(String[] args) throws Exception {
+		FindItemGoogleShopping fi =  new FindItemGoogleShopping();
+		fi.findItemGoogleShopping("Samsung Galaxy");
+
+	}
+
+}
