6dd3ca28f81c2857aee2e132736f5cced5bbf759
FKSI <ky.francois.3@gmail.com>
2013-05-31 18:40:53 +0200

    Beautful HTML

--- src/Client/ConcreteClient.java
+++ src/Client/ConcreteClient.java
@@ -47,22 +47,43 @@ public class ConcreteClient {
 		try{
 
 
-			File f = new File("source.htm");
+			File f = new File("index.html");
 			BufferedWriter bw = new BufferedWriter(new FileWriter(f));
 			bw.write("<html>");
+			bw.write("<head>");
+			bw.write("<meta charset='utf-8'>");
+			bw.write("<title>* DSMWare *</title>");
+			bw.write("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+			bw.write("<link href='bootstrap/css/bootstrap.css' rel='stylesheet'>");
+			bw.write("<link href='css/flat-ui.css' rel='stylesheet'>");
+			bw.write("</head>");
 			bw.write("<body>");
-			bw.write("<h1>ShowGeneratedHtml source</h1>");
-			bw.write("<textarea cols=75 rows=30>");
-			
-			while (!(line = in.readLine()).equals("\u0004")) {
-				//System.out.println(line);
-				bw.write(line);
-				bw.newLine();
-			}
+			bw.write("<div class='demo-headline'><h1 class='demo-logo'>DSMWare Project - Spring 2013<small>SMETKO Andrea - KY Francois</small></h1></div>");
+			bw.write("<div class='container'>");
 
-		
+			bw.write("<center><div class='span11'><a href='#Top' class='btn btn-large btn-block btn-info'>Go to the top of the list </a></div></center>");
+			bw.write("</br>");
+			bw.write("</br>");
+			bw.write("<div class='span11'>");
+			bw.write("<div class='demo-text-box prl'>");
+			bw.write("<a name='Top'>");
+			while (!(line = in.readLine()).equals("\u0004")) {
+				
+				bw.write("<div class='fui-radio-unchecked'></div>");
+				bw.write(" "+line);
+				bw.write("</br>");
+				bw.write("</br>");
+				//bw.newLine();
+			}
+			bw.write("<a name='Bot'>");
+			bw.write("</div>");
+			bw.write("<center><div class='span11'><a href='#Top' class='btn btn-large btn-block btn-primary'>Go to the top of the list </a></div></center>");
+			bw.write("</div>");
 			
-			bw.write("</text" + "area>");
+			bw.write("</div>");
+
+			
+			
 			bw.write("</body>");
 			bw.write("</html>");
 			bw.close();
@@ -70,7 +91,7 @@ public class ConcreteClient {
 			Desktop.getDesktop().browse(f.toURI());
 			
 			in.close();
-			socket.close();
+			//socket.close();
 			//System.out.println("Text received: " + line);
 		} catch (IOException e){
 			System.out.println("Read failed");
